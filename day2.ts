986a63065b620a02af8cb0d7f703b7cfd10220b6
javeri-a <160253455+javeri-a@users.noreply.github.com>
2024-03-06 18:22:01 +0500

    Add files via upload

---
+++ day2.ts
@@ -0,0 +1,22 @@
+// DAY 2 CHALLENGE: 
+
+//   QUESTION NUMBER 1
+// Question 1:Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation//
+let quote=("Predicting The Future Isn't Magic,Its Artificial Intelligence,")
+let author=("Dave Waters")
+let variable=("Once Said:")
+console.log("" .concat(author,variable,quote));
+console.log("\n");
+
+//   QUESTION NUMBER 2
+//Question 2:Famous Quote 2: Repeat Exercise 4, but this time store the famous person's name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.//
+let message = ("Predicting The Future Isn't Magic,Its Artificial Intelligence,");
+let famous_person = ("Dave Waters:");
+let said = ("once said,");
+console.log(" ".concat(famous_person, said, message));
+console.log("\n");
+//    QUESTION NUMBER 3
+//Question 3:Stripping Names: Store a personâ€™s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.//
+let personName =("\t\n javeria shahzad \t\n")
+console.log("stripping:", personName.trim());
+
